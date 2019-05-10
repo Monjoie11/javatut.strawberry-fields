3c8aae6f00f39835e6d58ffb26af56b6c891c355
Monjoie11 <jeff.franken@gmail.com>
2019-05-10 00:14:08 -0600

    secong java titorial now learning arrays and primitive data types

---
+++ src/StrawberryFields.java
@@ -0,0 +1,11 @@
+public class StrawberryFields {
+
+
+  static int ringo = 0;
+  static int paul = 1;
+  static boolean john = false;
+  static char george = String("awesomeness");
+
+  System.out.println(ringo, john, paul, george);
+
+}
